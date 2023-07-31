4e59de8a207dd6efb18abd44ae88a4c72e50539b
fancky2019 <709737588@qq.com>
2023-07-31 15:56:25 +0800

    client jwt 指定盐就不用到sso  去校验

---
+++ authorizationClient/src/main/java/com/fancky/authorizationclient/service/OauthClientDetailsService.java
@@ -0,0 +1,13 @@
+package com.fancky.authorizationclient.service;
+
+import com.fancky.authorizationclient.entity.OauthClientDetails;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author ruili
+* @description 针对表【oauth_client_details】的数据库操作Service
+* @createDate 2023-07-29 15:48:49
+*/
+public interface OauthClientDetailsService extends IService<OauthClientDetails> {
+
+}
