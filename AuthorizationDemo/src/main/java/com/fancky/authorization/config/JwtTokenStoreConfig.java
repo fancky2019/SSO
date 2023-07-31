03dfcbe7192ae85e0c3e3cd6909a36f8326c66af
fancky2019 <709737588@qq.com>
2023-07-31 16:02:21 +0800

    jwt 指定signkey不用到 sso 校验

--- AuthorizationDemo/src/main/java/com/fancky/authorization/config/JwtTokenStoreConfig.java
+++ AuthorizationDemo/src/main/java/com/fancky/authorization/config/JwtTokenStoreConfig.java
@@ -18,6 +18,8 @@ public class JwtTokenStoreConfig {
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+       //签名key要和客户端保持一直，也可以在客户端配置调取sso的盐
+       //盐是一个随机字符串,它与密码组合在一起,以生成一个唯一的哈希值。这个过程被称为“盐值哈希”
         accessTokenConverter.setSigningKey("JWT_Sign_Key");//配置JWT使用的秘钥。注意：要和客户端的保持一致
         return accessTokenConverter;
     }
